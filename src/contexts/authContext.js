424ca1ff19306246160f26d06e466c16a369a8fc
Alexandre Lima <alexandreatlima@gmail.com>
2022-05-10 10:32:56 -0300

    first commit

---
+++ src/contexts/authContext.js
@@ -0,0 +1,15 @@
+import { createContext, useState } from "react";
+
+const AuthContext = createContext({ token: "", user: {} });
+
+function AuthContextComponent(props) {
+  const [loggedInUser, setLoggedInUser] = useState({ token: "", user: {} });
+
+  return (
+    <AuthContext.Provider value={{ loggedInUser, setLoggedInUser }}>
+      {props.children}
+    </AuthContext.Provider>
+  );
+}
+
+export { AuthContext, AuthContextComponent };
---
+++ src/pages/Signup/index.js
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import { api } from "../../api/api";
+import { useNavigate } from "react-router-dom";
+
+export function Signup() {
+  const navigate = useNavigate();
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  function handleChange(e) {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    try {
+      await api.post("/user/signup", form);
+
+      navigate("/login");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="formName">Nome:</label>
+      <input
+        id="formName"
+        name="name"
+        type="text"
+        value={form.name}
+        onChange={handleChange}
+      />
+      <label htmlFor="formEmail">E-mail:</label>
+      <input
+        id="formEmail"
+        name="email"
+        type="email"
+        value={form.email}
+        onChange={handleChange}
+      />
+      <label htmlFor="formPassword">Senha:</label>
+      <input
+        id="formPassword"
+        name="password"
+        type="password"
+        value={form.password}
+        onChange={handleChange}
+      />
+      <label htmlFor="formConfirmPassword">Confirmação de senha</label>
+      <input
+        id="formConfirmPassword"
+        type="password"
+        name="confirmPassword"
+        value={form.confirmPassword}
+        onChange={handleChange}
+      />
+      <button type="submit">Cadastrar</button>
+    </form>
+  );
+}
