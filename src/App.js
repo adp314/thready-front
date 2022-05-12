f24b8e2d6d6bca7711c5076cb0ce4a2cda859b09
Alexandre Lima <alexandreatlima@gmail.com>
2022-05-12 09:27:22 -0300

    protectedroute and errorpage

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import { Login } from "./pages/Login";
 import { Signup } from "./pages/Signup";
 import { AuthContextComponent } from "./contexts/authContext";
 import { Profile } from "./pages/Profile";
+import { ErrorPage } from "./pages/ErrorPage";
+
+import { ProtectedRoute } from "./components/ProtectedRoute";
+
 function App() {
   return (
     <>
@@ -12,7 +16,12 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/profile" element={<Profile />} />
+          <Route
+            path="/profile"
+            element={<ProtectedRoute component={Profile} />}
+          />
+
+          <Route path="*" element={<ErrorPage />} />
         </Routes>
       </AuthContextComponent>
     </>
