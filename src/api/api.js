a6ec357eedf3d806a233b3a1035e76b42b09e282
Vitor Moraes <vitoristvan@hotmail.com>
2022-12-16 12:18:32 -0300

    url production added

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiURLs = {
   development: "http://localhost:4000/api/1.0",
-  production: "LINK DA SUA API DEPLOYADA AQUI!",
+  production: "https://thready-back.cyclic.app",
 };
 
 const api = axios.create({ baseURL: apiURLs[process.env.NODE_ENV] });
